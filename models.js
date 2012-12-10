bf4d25b4b40c5bf67822a385e619efd673480996
Mitar <mitar.git@tnode.com>
2012-12-09 21:16:59 -0800

    Moved models to external file.

---
+++ models.js
@@ -0,0 +1,85 @@
+var mongoose = require('mongoose');
+
+var $ = require('jquery');
+
+var settings = require('./settings');
+
+var db = mongoose.createConnection(settings.MONGODB_URL).on('error', function (err) {
+    // TODO: Handle (just throw an exception and let us be respawned?)
+    console.error("MongoDB connection error: %s", err);
+}).once('open', function () {
+    console.log("MongoDB connection successful");
+});
+
+var schema = mongoose.Schema({
+    'type': {
+        'type': String,
+        'required': true
+    },
+    'foreign_id': {
+        'type': String,
+        'unique': true,
+        'required': true
+    },
+    'foreign_timestamp': {
+        'type': Date,
+        'index': true,
+        'required': true
+    },
+    'data': {
+        'type': mongoose.Schema.Types.Mixed,
+        'required': true
+    },
+    'original_data': {
+        'type': mongoose.Schema.Types.Mixed,
+        'required': false
+    }
+});
+var Post = db.model('Post', schema);
+
+function storePost(foreign_id, type, foreign_timestamp, data, original_data) {
+    var query = {'foreign_id': foreign_id, 'type': type};
+    Post.findOneAndUpdate(query, {'foreign_timestamp': foreign_timestamp, 'data': data, 'original_data': original_data}, {'upsert': true, 'new': false}, function (err, obj) {
+        if (err) {
+            console.error("Post (%s/%s) store error: %s", type, foreign_id, err);
+            return;
+        }
+
+        if (!obj.toObject()) {
+            // Post was not already stored
+            // We load post manually, because to know if post was stored or not we
+            // do not set "new" parameter of findOneAndUpdate call
+            Post.findOne(query, {'type': true, 'foreign_id': true, 'foreign_timestamp': true, 'data': true}).lean(true).exec(function (err, post) {
+                if (err) {
+                    console.error("Post (%s/%s) load error: %s", type, foreign_id, err);
+                    return;
+                }
+
+                post.fetch_timestamp = post._id.getTimestamp();
+                delete post._id;
+
+                $.each(clients, function (i, client) {
+                    client.newPost(post);
+                });
+            });
+        }
+    });
+}
+
+function storeTweet(tweet) {
+    var data = {
+        'from_user': tweet.from_user || tweet.user.screen_name,
+        'in_reply_to_status_id': tweet.in_reply_to_status_id,
+        'text': tweet.text
+    };
+
+    storePost(tweet.id_str, 'twitter', new Date(tweet.created_at), data, tweet);
+}
+
+function storeFacebookPost(post) {
+    storePost(post.id, 'facebook', new Date(post.created_time), post, null);
+}
+
+exports.Post = Post;
+exports.storeTweet = storeTweet;
+exports.storeFacebookPost = storeFacebookPost;
--- web.js
+++ web.js
@@ -3,7 +3,6 @@ var url = require('url');
 var shoe = require('shoe');
 var ecstatic = require('ecstatic')(__dirname + '/static');
 var dnode = require('dnode');
-var mongoose = require('mongoose');
 var twitter = require('ntwitter');
 var swig = require('swig');
 var request = require('request');
@@ -11,39 +10,7 @@ var request = require('request');
 var $ = require('jquery');
 
 var settings = require('./settings');
-
-var db = mongoose.createConnection(settings.MONGODB_URL).on('error', function (err) {
-    // TODO: Handle (just throw an exception and let us be respawned?)
-    console.error("MongoDB connection error: %s", err);
-}).once('open', function () {
-    console.log("MongoDB connection successful");
-});
-
-var schema = mongoose.Schema({
-    'type': {
-        'type': String,
-        'required': true
-    },
-    'foreign_id': {
-        'type': String,
-        'unique': true,
-        'required': true
-    },
-    'foreign_timestamp': {
-        'type': Date,
-        'index': true,
-        'required': true
-    },
-    'data': {
-        'type': mongoose.Schema.Types.Mixed,
-        'required': true
-    },
-    'original_data': {
-        'type': mongoose.Schema.Types.Mixed,
-        'required': false
-    }
-});
-var Post = db.model('Post', schema);
+var models = require('./models');
 
 swig.init({
     'root': __dirname + '/templates'
@@ -113,11 +80,11 @@ var sock = shoe(function (stream) {
             }
             // TODO: Implement support for start
             // TODO: Once implemented, make client load new posts when it scrolls to the end of the page
-			Post.find({}, {'type': true, 'foreign_id': true, 'foreign_timestamp': true, 'data': true}).sort({'foreign_timestamp': 'desc'}).limit(limit).lean(true).exec(function (err, posts) {
-				if (err) {
-					cb(err);
+            models.Post.find({}, {'type': true, 'foreign_id': true, 'foreign_timestamp': true, 'data': true}).sort({'foreign_timestamp': 'desc'}).limit(limit).lean(true).exec(function (err, posts) {
+                if (err) {
+                    cb(err);
                     return;
-				}
+                }
 
                 posts = $.map(posts, function (post, i) {
                     post.fetch_timestamp = post._id.getTimestamp();
@@ -125,8 +92,8 @@ var sock = shoe(function (stream) {
                     return post;
                 });
 
-				cb(null, posts);
-			});
+                cb(null, posts);
+            });
         }
     });
     d.on('remote', function (remote, d) {
@@ -146,51 +113,12 @@ var twit = new twitter({
     'access_token_secret': settings.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-function storePost(foreign_id, type, foreign_timestamp, data, original_data) {
-    var query = {'foreign_id': foreign_id, 'type': type};
-    Post.findOneAndUpdate(query, {'foreign_timestamp': foreign_timestamp, 'data': data, 'original_data': original_data}, {'upsert': true, 'new': false}, function (err, obj) {
-        if (err) {
-            console.error("Post (%s/%s) store error: %s", type, foreign_id, err);
-            return;
-        }
-
-        if (!obj.toObject()) {
-            // Post was not already stored
-            // We load post manually, because to know if post was stored or not we
-            // do not set "new" parameter of findOneAndUpdate call
-            Post.findOne(query, {'type': true, 'foreign_id': true, 'foreign_timestamp': true, 'data': true}).lean(true).exec(function (err, post) {
-                if (err) {
-                    console.error("Post (%s/%s) load error: %s", type, foreign_id, err);
-                    return;
-                }
-
-                post.fetch_timestamp = post._id.getTimestamp();
-                delete post._id;
-
-                $.each(clients, function (i, client) {
-                    client.newPost(post);
-                });
-            });
-        }
-    });
-}
-
-function storeTweet(tweet) {
-    var data = {
-        'from_user': tweet.from_user || tweet.user.screen_name,
-        'in_reply_to_status_id': tweet.in_reply_to_status_id,
-        'text': tweet.text
-    };
-
-    storePost(tweet.id_str, 'twitter', new Date(tweet.created_at), data, tweet);
-}
-
 function connectToTwitterStream() {
     console.log("Twitter stream connecting");
     twit.stream('statuses/filter', {'track': settings.TWITTER_QUERY}, function (stream) {
         console.log("Twitter stream connected");
         stream.on('data', function (data) {
-            storeTweet(data);
+            models.storeTweet(data);
         }).on('delete', function (data) {
             // TODO: Implement (https://dev.twitter.com/docs/streaming-apis/messages)
             console.log("Twitter delete: %s", data);
@@ -218,17 +146,13 @@ function fetchTwitterLatest() {
         }
 
         $.each(data.results, function (i, tweet) {
-            storeTweet(tweet);
+            models.storeTweet(tweet);
         });
     });
 }
 
 fetchTwitterLatest();
 connectToTwitterStream();
-
-function storeFacebookPost(post) {
-    storePost(post.id, 'facebook', new Date(post.created_time), post, null);
-}
 
 function fetchFacebookLatest(limit) {
     request('https://graph.facebook.com/' + settings.FACEBOOK_PAGE_ID + '/tagged?access_token=' + settings.FACEBOOK_ACCESS_TOKEN + '&limit=' + limit, function (error, res, body) {
@@ -246,7 +170,7 @@ function fetchFacebookLatest(limit) {
         }
 
         $.each(body.data, function (i, post) {
-            storeFacebookPost(post);
+            models.storeFacebookPost(post);
         });
     });
 }

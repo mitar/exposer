ba7e09805944f575473fdc67cc100698d955aa06
Mitar <mitar.git@tnode.com>
2012-12-09 21:19:00 -0800

    Added another common tag.

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ exports.FACEBOOK_PAGE_ID = process.env.FACEBOOK_PAGE_ID;
 exports.FACEBOOK_ACCESS_TOKEN = process.env.FACEBOOK_ACCESS_TOKEN;
 exports.FACEBOOK_REALTIME_VERIFY_TOKEN = process.env.FACEBOOK_REALTIME_VERIFY_TOKEN;
 
-exports.TWITTER_QUERY = ['#gotofje', '#gotofsi', '#protesti', '@gotofsi', '@gotofje', '#gotoviso', '#mbprotest', '#ljprotest'];
+exports.TWITTER_QUERY = ['#gotofje', '#gotofsi', '#protesti', '@gotofsi', '@gotofje', '#gotoviso', '#mbprotest', '#ljprotest', '#fertikje'];
 exports.FACEBOOK_REALTIME_PATHNAME = '/fb/realtime';
 exports.MAX_POSTS_PER_REQUEST = 50;
 exports.FACEBOOK_POLL_INTERVAL = 3 * 60 * 1000; // ms

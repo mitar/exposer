a37ba2297baad6c55ef9db7a7143ea3575c2e3b9
Mitar <mitar.git@tnode.com>
2013-01-21 22:20:33 -0800

    Merge duplicate Facebook posts. Fixes #14

--- mergeposts.js
+++ mergeposts.js
@@ -1,89 +1,18 @@
 var async = require('async');
-var moment = require('moment');
 
 var _ = require('underscore');
 
 var models = require('./models');
-var settings = require('./settings');
 
 var POST_EQUALITY_FIELDS = {};
 _.each(models.Post.EQUALITY_FIELDS, function (value, field, list) {
     POST_EQUALITY_FIELDS[field.replace('.', '_')] = '$' + field;
 });
 
-function compare(a, b) {
-    // We prefer more public posts
-    if (a.data.actions && !b.data.actions) {
-        return -1;
-    }
-    else if (!a.data.actions && b.data.actions) {
-        return 1;
-    }
-    // We prefer posts with more likes
-    else if (a.data.likes && !b.data.likes) {
-        return -1;
-    }
-    else if (!a.data.likes && b.data.likes) {
-        return 1;
-    }
-    else if (a.data.likes && _.isFinite(a.data.likes.count) && b.data.likes && _.isFinite(b.data.likes.count)) {
-        return b.data.likes.count - a.data.likes.count;
-    }
-    // We prefer posts with more comments
-    else if (a.data.comments && !b.data.comments) {
-        return -1;
-    }
-    else if (!a.data.comments && b.data.comments) {
-        return 1;
-    }
-    else if (a.data.comments && _.isFinite(a.data.comments.count) && b.data.comments && _.isFinite(b.data.comments.count)) {
-        return b.data.comments.count - a.data.comments.count;
-    }
-    // We prefer posts which have story
-    if (a.data.story && !b.data.story) {
-        return -1;
-    }
-    else if (!a.data.story && b.data.story) {
-        return 1;
-    }
-    // We prefer bigger posts
-    var size = _.size(b) - _.size(a);
-    if (size !== 0) {
-        return size;
-    }
-    // We prefer older (original, first) posts
-    var created_a = moment(a.data.created_time);
-    var created_b = moment(b.data.created_time);
-    if (created_a < created_b) {
-        return -1;
-    }
-    else if (created_b < created_a) {
-        return 1;
-    }
-    if (a.data.updated_time && !b.data.updated_time) {
-        return -1;
-    }
-    else if (!a.data.updated_time && b.data.updated_time) {
-        return 1;
-    }
-    else if (a.data.updated_time && b.data.updated_time) {
-        // We prefer posts which were updated recently
-        var updated_a = moment(a.data.updated_time);
-        var updated_b = moment(b.data.updated_time);
-        if (updated_a < updated_b) {
-            return 1;
-        }
-        else if (updated_b < created_a) {
-            return -1;
-        }
-    }
-    return 0;
-}
-
 function mergeposts() {
     models.Post.aggregate([
         {'$match': {'type': 'facebook'}},
-        {'$group': {'_id': POST_EQUALITY_FIELDS, 'count': {'$sum': 1}, 'posts': {'$push': {'foreign_id': '$foreign_id', 'data': '$data', 'sources': '$sources', 'merged_from': '$merged_from'}}}},
+        {'$group': {'_id': POST_EQUALITY_FIELDS, 'count': {'$sum': 1}, 'posts': {'$push': {'foreign_id': '$foreign_id', 'data': '$data'}}}},
         {'$match': {'count': {'$gt': 1}}}
     ], function (err, results) {
         if (err) {
@@ -93,36 +22,15 @@ function mergeposts() {
         }
 
         async.forEach(results, function (result, cb) {
-            var posts = result.posts.sort(compare);
-
-            var first_id = _.first(posts).foreign_id;
-
-            var rest = _.rest(posts);
-            var rest_ids = _.pluck(rest, 'foreign_id');
-
-            models.Post.update({'foreign_id': first_id}, {'$addToSet': {'merged_from': rest_ids}, '$unset': {'merged_to': true}}, {'multi': true}, function (err, numberAffected, rawResponse) {
+            models.Post.doMerge(result.posts, function (err, first_id, rest_ids) {
                 if (err) {
-                    console.error(err);
-                }
-                else if (numberAffected !== 1) {
-                    console.error("Invalid number of Facebook posts set as main: %s", numberAffected, first_id, rawResponse);
+                    console.log(err);
                 }
                 else {
-                    models.Post.update({'foreign_id': {'$in': rest_ids}}, {'$set': {'merged_to': first_id}, '$unset': {'merged_from': true}}, {'multi': true}, function (err, numberAffected, rawResponse) {
-                        if (err) {
-                            console.error(err);
-                        }
-                        else if (numberAffected !== rest_ids.length) {
-                            console.error("Invalid number of Facebook posts set as merged: %s/%s", numberAffected, rest_ids.length, first_id, rest_ids, rawResponse);
-                        }
-                        else {
-                            console.log("Merged Facebook posts: %s -> %s", rest_ids, first_id);
-                        }
-
-                        // We handle error independently
-                        cb(null);
-                    });
+                    console.log("Merged Facebook posts: %s -> %s", rest_ids, first_id);
                 }
+                // We handle error independently
+                cb(null);
             });
         }, function (err) {
             process.exit(0);
--- models.js
+++ models.js
@@ -104,6 +104,8 @@ postSchema.statics.PUBLIC_FIELDS = {
     'facebook_event_id': true
 };
 
+// Must be a subset of PUBLIC_FIELDS and should not contain fields
+// which are removed in "cleanPost" for "merge" function bellow to work
 postSchema.statics.EQUALITY_FIELDS = {
     'data.from': true,
     'data.to': true,
@@ -145,7 +147,169 @@ postSchema.statics.storeTweet = function (tweet, source, cb) {
         'text': tweet.text
     };
 
-    storePost(tweet.id_str, 'twitter', moment(tweet.created_at).toDate(), source, data, tweet, cb);
+    storePost(tweet.id_str, 'twitter', moment(tweet.created_at).toDate(), source, data, tweet, function (err, tweet) {
+        if (err) {
+            cb(err);
+            return;
+        }
+
+        if (!tweet) {
+            cb(null, null);
+            return;
+        }
+
+        delete tweet.facebook_event_id;
+        cb(null, tweet);
+    });
+};
+
+// Not all post fields are necessary available, depending on the projection in "mergeposts" function
+function compare(a, b) {
+    // We prefer more public posts
+    if (a.data.actions && !b.data.actions) {
+        return -1;
+    }
+    else if (!a.data.actions && b.data.actions) {
+        return 1;
+    }
+    // We prefer posts with more likes
+    else if (a.data.likes && !b.data.likes) {
+        return -1;
+    }
+    else if (!a.data.likes && b.data.likes) {
+        return 1;
+    }
+    else if (a.data.likes && _.isFinite(a.data.likes.count) && b.data.likes && _.isFinite(b.data.likes.count)) {
+        return b.data.likes.count - a.data.likes.count;
+    }
+    // We prefer posts with more comments
+    else if (a.data.comments && !b.data.comments) {
+        return -1;
+    }
+    else if (!a.data.comments && b.data.comments) {
+        return 1;
+    }
+    else if (a.data.comments && _.isFinite(a.data.comments.count) && b.data.comments && _.isFinite(b.data.comments.count)) {
+        return b.data.comments.count - a.data.comments.count;
+    }
+    // We prefer posts which have story
+    if (a.data.story && !b.data.story) {
+        return -1;
+    }
+    else if (!a.data.story && b.data.story) {
+        return 1;
+    }
+    // We prefer bigger posts
+    var size = _.size(b) - _.size(a);
+    if (size !== 0) {
+        return size;
+    }
+    // We prefer older (original, first) posts
+    var created_a = moment(a.data.created_time);
+    var created_b = moment(b.data.created_time);
+    if (created_a < created_b) {
+        return -1;
+    }
+    else if (created_b < created_a) {
+        return 1;
+    }
+    if (a.data.updated_time && !b.data.updated_time) {
+        return -1;
+    }
+    else if (!a.data.updated_time && b.data.updated_time) {
+        return 1;
+    }
+    else if (a.data.updated_time && b.data.updated_time) {
+        // We prefer posts which were updated recently
+        var updated_a = moment(a.data.updated_time);
+        var updated_b = moment(b.data.updated_time);
+        if (updated_a < updated_b) {
+            return 1;
+        }
+        else if (updated_b < created_a) {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+postSchema.statics.doMerge = function (posts, cb) {
+    posts = posts.sort(compare);
+
+    var first_id = _.first(posts).foreign_id;
+
+    var rest = _.rest(posts);
+    var rest_ids = _.pluck(rest, 'foreign_id');
+
+    Post.update({'foreign_id': first_id}, {'$addToSet': {'merged_from': rest_ids}, '$unset': {'merged_to': true}}, {'multi': true}, function (err, numberAffected, rawResponse) {
+        if (err) {
+            cb("Error merging posts: " + err);
+            return;
+        }
+        else if (numberAffected !== 1) {
+            cb("Invalid number of Facebook posts set as main (" + first_id + ", " + numberAffected + "): " + rawResponse);
+            return;
+        }
+
+        Post.update({'foreign_id': {'$in': rest_ids}}, {'$set': {'merged_to': first_id}, '$unset': {'merged_from': true}}, {'multi': true}, function (err, numberAffected, rawResponse) {
+            if (err) {
+                cb("Error merging posts: " + err);
+                return;
+            }
+            else if (numberAffected !== rest_ids.length) {
+                cb("Invalid number of Facebook posts set as merged (" + first_id + ", " + rest_ids + ", " + numberAffected + "/" + rest_ids.length + "): " + rawResponse);
+                return;
+            }
+
+            cb(null, first_id, rest_ids);
+        });
+    });
+};
+
+postSchema.statics.merge = function (post, cb) {
+    var query = {};
+
+    // We assume "post" contains all EQUALITY_FIELDS fields if they do exist in the database for it
+    _.each(Post.EQUALITY_FIELDS, function (value, field, list) {
+        var v = _.reduce(field.split('.'), function (memo, f) {
+            return _.isUndefined(memo) ? undefined : memo[f];
+        }, post);
+        if (!_.isUndefined(v)) {
+            query[field] = v;
+        }
+    });
+
+    Post.find(query).lean(true).exec(function (err, posts) {
+        if (err) {
+            // Just log the error and continue
+            console.error("Error while merging post (%s): %s", post.foreign_id, err);
+            cb(null, false);
+            return;
+        }
+
+        if (posts.length < 2) {
+            // Nothing to merge
+            cb(null, false);
+            return;
+        }
+
+        Post.doMerge(posts, function (err, first_id, rest_ids) {
+            if (err) {
+                // Just log the error and continue
+                console.error("Error while merging post (%s): %s", post.foreign_id, err);
+                cb(null, false);
+                return;
+            }
+
+            if (_.indexOf(rest_ids, post.foreign_id) !== -1) {
+                // Post was merged into one another, we set "post_merged" to true, so that it is not send further in the callback
+                cb(null, true);
+            }
+            else {
+                cb(null, false);
+            }
+        });
+    });
 };
 
 postSchema.statics.storeFacebookPost = function (post, source, cb) {
@@ -160,27 +324,34 @@ postSchema.statics.storeFacebookPost = function (post, source, cb) {
             return;
         }
 
-        var new_event = !callback_post.facebook_event_id;
-        delete callback_post.facebook_event_id;
+        Post.merge(callback_post, function (err, post_merged) {
+            if (err) {
+                cb(err);
+                return;
+            }
 
-        // We check callback_post here, too, to optimize database access
-        if (Post.hasEvent(callback_post)) {
-            // We fetch Facebook event for the first time or update existing (if multiple posts link to the same event, for example)
-            Post.fetchFacebookEvent(post.id, function (err, event) {
-                if (err) {
-                    // Just log the error and continue
-                    console.error(err);
-                }
+            var new_event = !callback_post.facebook_event_id;
+            delete callback_post.facebook_event_id;
 
-                event = event || null;
+            // We check callback_post here, too, to optimize database access
+            if (Post.hasEvent(callback_post)) {
+                // We fetch Facebook event for the first time or update existing (if multiple posts link to the same event, for example)
+                Post.fetchFacebookEvent(post.id, function (err, event) {
+                    if (err) {
+                        // Just log the error and continue
+                        console.error(err);
+                    }
 
-                callback_post.facebook_event = event;
-                cb(null, callback_post, new_event ? event : null);
-            });
-        }
-        else {
-            cb(null, callback_post, null);
-        }
+                    event = event || null;
+
+                    callback_post.facebook_event = event;
+                    cb(null, post_merged ? null : callback_post, new_event ? event : null);
+                });
+            }
+            else {
+                cb(null, post_merged ? null : callback_post, null);
+            }
+        });
     });
 };
 
@@ -188,7 +359,7 @@ postSchema.statics.createTypeForeignId = function (type, foreign_id) {
     return type + '/' + foreign_id;
 };
 
-// If existing, "facebook_event_id" is left and should be exchanged for real data ("facebook_event") or deleted
+// "facebook_event_id" should be exchanged for real data ("facebook_event") or deleted
 postSchema.statics.cleanPost = function (post) {
     post.fetch_timestamp = post._id.getTimestamp();
     delete post._id;
@@ -202,11 +373,6 @@ postSchema.statics.cleanPost = function (post) {
     }
     if (post.data && post.data.shares && post.data.shares.data) {
         delete post.data.shares.data;
-    }
-
-    if (!post.facebook_event_id) {
-        // If false, null, or non-existent, we remove it (so that it is not available in tweets, for example
-        delete post.facebook_event_id;
     }
 
     return post;

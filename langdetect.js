7379f09911e4124e56f958773e761d8cd712a512
Mitar <mitar.git@tnode.com>
2013-01-28 10:50:19 -0800

    Count detected languages.

--- langdetect.js
+++ langdetect.js
@@ -5,6 +5,8 @@ var _ = require('underscore');
 var models = require('./models');
 
 function langdetect() {
+    var all = 0;
+    var languages = 0;
     models.Post.find({'type': 'facebook'}).exec(function (err, posts) {
         if (err) {
             console.error(err);
@@ -13,7 +15,14 @@ function langdetect() {
         }
 
         async.forEach(posts, function (post, cb) {
+            all++;
+
             post.language = models.Post.detectLanguage(post.type, post.data);
+
+            if (post.language) {
+                languages++;
+            }
+
             post.save(function (err, obj) {
                 if (err) {
                     console.error(err);
@@ -22,6 +31,7 @@ function langdetect() {
                 cb(null);
             });
         }, function (err) {
+            console.log("Detected non-target language on %s of %s posts", languages, all);
             process.exit(0);
         });
     });

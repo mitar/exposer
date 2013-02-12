5a427e2f34d074b630a590690d8fc3c96cf16631
Mitar <mitar.git@tnode.com>
2013-02-12 01:37:10 -0800

    Minimize JavaScript only on production.

--- configure.js
+++ configure.js
@@ -6,10 +6,12 @@ module.exports = function (bundle) {
     bundle.register('.html', function (body, file) {
         return "module.exports = function (swig) {return swig.compile('" + filters.escape(body, 'js') + "', {'filename': '" + filters.escape(path.relative('./templates/', file), 'js') + "'})};";
     });
-    bundle.register('post', function (body) {
-        return uglify.minify(body, {
-            'fromString': true
-        }).code;
-    });
+    if (process.env.NODE_ENV === 'production') {
+        bundle.register('post', function (body) {
+            return uglify.minify(body, {
+                'fromString': true
+            }).code;
+        });
+    }
     bundle.require('jquery-browserify', {'target': 'jquery' });
 };

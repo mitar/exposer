f7e098a10e09f8a8421cba0c8a128fba34c637f2
Mitar <mitar.git@tnode.com>
2013-02-11 20:19:02 -0800

    Minify JavaScript code.

--- configure.js
+++ configure.js
@@ -1,9 +1,15 @@
 var filters = require('swig/lib/filters');
 var path = require('path');
+var uglify = require('uglify-js');
 
 module.exports = function (bundle) {
     bundle.register('.html', function (body, file) {
         return "module.exports = function (swig) {return swig.compile('" + filters.escape(body, 'js') + "', {'filename': '" + filters.escape(path.relative('./templates/', file), 'js') + "'})};";
+    });
+    bundle.register('post', function (body) {
+        return uglify.minify(body, {
+            'fromString': true
+        }).code;
     });
     bundle.require('jquery-browserify', {'target': 'jquery' });
 };
